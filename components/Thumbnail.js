bba63f966884ed39862b9d629b4f395b0bb48d5b
Alihan Cibooğlu <alihanc@Alihan-MacBook-Pro.local>
2022-06-10 23:59:34 +0300

    vercel build bug fixed

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import { ThumbUpIcon } from '@heroicons/react/outline';
 import { forwardRef } from 'react';
 
 
-const  Thumbnail = forwardRef(({ result }, ref) =>  {
+const Thumbnail = forwardRef(({ result }, ref) =>  {
     const BASE_URL = "https://image.tmdb.org/t/p/original"
   return (
     <div ref={ref} className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
@@ -30,4 +30,4 @@ const  Thumbnail = forwardRef(({ result }, ref) =>  {
   )
 })
 
-export default Thumbnail
+export default Thumbnail;
